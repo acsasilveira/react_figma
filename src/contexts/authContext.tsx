b6577906aed9558b693007e3118bead6ee990604
acsasilveira <acsa.silveiras@gmail.com>
2022-09-23 08:37:56 -0300

    Versao CEFET - 23-09

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -10,3 +10,59 @@ import { useNAvigate } from 'react-router-dom'
 
 const AuthContext = createContext<IAuthContextData>({} as IAuthContextData)
 
+const AuthProvider: React.FC<IAuthProvider> = ({ children }) => {
+    const [auth, setAuth] = useState<IResponseUser>({} as IResponseUser)
+    const navigate = useNavigate();
+
+    const signIn = useCallback(async ({ email, password }: IUser) => {
+        const response = await apiUser.login({ email, password })
+        const { token, user } = response.data
+        api.defaults.headers.common.Authorization = `Bearer ${token}`
+        setAuth({ token, user})
+
+        localStorage.setItem('@web1:token', JSON.stringify(token))
+        localStorage.setItem('@web1:user', JSON.stringify(user))
+    }, [])
+
+    const removeLocalStorage = useCallback(async () => {
+        localStorage.removeItem('@web1:token')
+        localStorage.removeItem('@web1:user')
+    }, [])
+
+    const signOut = useCallback(async () => {
+        setAuth({} as IResponseUser)
+        await removeLocalStorage()
+        delete api.defaults.headers.common.Authorization
+        navigate('/login')
+    }, [removeLocalStorage, navigate])
+
+    const loadUserStorageData = useCallback(async () => {
+        const token = JSON.parse(String(localStorage.getItem('@web1:token')))
+        const user = JSON.parse(String(localStorage.getItem('@web1:user')))
+        if (token && user){
+            api.defaults.headers.common.Authorization = `Bearer ${token.token}`
+            setAuth({ token, user })
+            if (isAfter(parseISO(token.expires_at), new Date())) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }, [])
+
+    return (
+        <AuthContext.Provider
+            value={{
+                signIn,
+                signOut,
+                loadUserStorageData,
+                user: auth.user,
+            }}
+            >
+                {children}
+        </AuthContext.Provider>
+    )
+}
+export { AuthProvider, AuthContext }
---
+++ src/hooks/authHook.tsx
@@ -0,0 +1,13 @@
+import { AuthContext } from "contexts/authContext"
+import { IAuthContextData } from "interfaces/user.interface"
+import { useContext } from "react"
+
+fuction useAuth(): IAuthContextData {
+    const context = useContext(AuthContext)
+    if (!context) {
+        throw new Error('useAuth deve ser utilizado com o AuthProvider')
+    }
+    return context
+}
+
+export  { useAuth }
