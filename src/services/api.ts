cbaad4e4b0e1aded23f12b2f8016e2c89abbfa04
acsasilveira <acsa.silveiras@gmail.com>
2022-11-21 14:21:00 -0300

    ...

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3333',
+    baseURL: process.env.REACT_APP_URL_API,
     headers: {
         'Content-Type' : 'application/json',
     },
